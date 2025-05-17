2246529434b9d15f07dc83c056051d806481fdd6
rpfarish <rpfarish@live.com>
2025-05-17 10:50:30 -0400

    added autopep8 to account for black being unopinonated about pep 8: E201 and E202

--- lua/rpfarish/lazy/init.lua
+++ lua/rpfarish/lazy/init.lua
@@ -307,6 +307,7 @@ return {
 				markdownlint = {},
 				isort = {},
 				black = {},
+				autopep8 = {},
 				prettierd = {},
 				prettier = {},
 				pyright = {},
@@ -404,7 +405,7 @@ return {
 				markdown = { "prettierd", "prettier" },
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "black" },
+				python = { "autopep8", "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				javascript = { "prettierd", "prettier", stop_after_first = true },
